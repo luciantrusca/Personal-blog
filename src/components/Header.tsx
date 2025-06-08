98ca90a4aa9ae9d6d026a6454b1baad57122594c
bobicez <bobicemj@gmail.com>
2025-06-08 19:15:47 +0200

    -finished header react component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,19 @@
+import "/src/app/globals.css"
+
 function Header(){
     return (
-    <header className="header">
-            <div className="header-logo">
-                <span className="logo-text">Chronicles of Curiosity</span>
-                <img src={}>★</img>
-                <nav className="header-nav">
-                    <a href="#latest-post">Latest post</a>
-                    <a href="#recent-posts">Recent posts</a>
-                    <a href="#about">About</a>
+    <header className="">
+            <div className="flex items-center space-x-2">   {/* Header logo text + svg + nav */}
+                <div className="flex items-center">
+                    <span className="">Chronicles of Curiosity</span>  {/* Header logo text*/}
+                    <img className="h-8 w-auto" src='/star.svg' alt="star" />           {/* Header svg */}
+                </div>
+                <nav className="">                      {/* Header navigation bar */}
+                    <ul className="flex space-x-2.5">
+                        <li><a href="#latest-post">Latest post</a></li>
+                        <li><a href="#recent-posts">Recent posts</a></li>
+                        <li><a href="#about">About</a></li>
+                    </ul>
                 </nav>
             </div>
         </header>
